acd0f1e047240e440890ef20ef88aec2861154d9
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-05-10 17:30:30 +0800

    Array
    Array functions.

--- Array/arr.js
+++ Array/arr.js
@@ -1,22 +1,22 @@
-function isFn(fun) {
-	return typeof fun === "function"
+function isFn(func) {
+    return typeof func === "function"
 }
 
 function isArray(obj) {
-	if (typeof obj !== "object" || obj === null) {
-		return false;
-	}
-	if (isFn(Array.isArray)) {
-		return Array.isArray(obj);
-	} else {
-		return Object.prototype.toString.call(obj) === "[object Array]";
-	}
+    if (typeof obj !== "object" || obj === null) {
+        return false;
+    }
+    if (isFn(Array.isArray)) {
+        return Array.isArray(obj);
+    } else {
+        return Object.prototype.toString.call(obj) === "[object Array]";
+    }
 }
 
 Array.append = function(arr, value) {
-	if (!isArray(arr)) {
-		return arr;
-	}
-	arr[arr.length] = value;
-	return arr;
+    if (!isArray(arr)) {
+        return arr;
+    }
+    arr[arr.length] = value;
+    return arr;
 }
