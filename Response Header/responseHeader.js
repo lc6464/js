ffff0793d579373dcd692161c956b5ef559b8a97
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-05-10 16:59:36 +0800

    Response Header
    Response Header functions.

---
+++ Response Header/responseHeader.js
@@ -0,0 +1,19 @@
+function getResponseHeaders() {
+	var req = new XMLHttpRequest();
+	req.open('GET', document.location, false);
+	req.send();
+	var headers = req.getAllResponseHeaders();
+	var arr = headers.trim().split(/[\r\n]{1,2}/),
+		headerMap = {};
+	arr.forEach(function(line) {
+		var parts = line.split(': '),
+			header = parts.shift(),
+			value = parts.join(': ');
+		headerMap[header] = value;
+	});
+	return headerMap;
+}
+
+function getResponseHeader(key) {
+	return getResponseHeaders()[key.toLowerCase()];
+}
