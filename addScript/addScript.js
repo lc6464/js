44e3d14c4c091d6d1d2598e6d26d47ac4855acaa
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-06-20 10:39:28 +0800

    Update addScript.js

--- addScript/addScript.js
+++ addScript/addScript.js
@@ -1,11 +1,14 @@
-function createScript(src){
-	var element = document.createElement('script');
-	element.src = src;
-	return element;
+function createScript(src, success, error, abort) {
+    var element = document.createElement('script');
+    element.src = src;
+    element.onload = success;
+    element.onerror = error;
+    element.onabort = abort;
+    return element;
 }
 
-function addScript(src){
-	var element = createScript(src);
-	document.head.appendChild(element);
-	return element;
+function addScript(src, success, error, abort) {
+    var element = createScript(src, success, error, abort);
+    document.head.appendChild(element);
+    return element;
 }
