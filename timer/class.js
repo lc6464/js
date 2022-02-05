5900db48c423348c9829c5462d0c0bea938ccb67
lc6464 <64722907+lc6464@users.noreply.github.com>
2022-02-05 21:10:04 +0800

    更新 Timer

--- timer/class.js
+++ timer/class.js
@@ -1,9 +1,17 @@
-class Timer { // ES2015 class 版本
-	constructor(time) { this.base = new Date(time == null ? Date.now() : time); } // 实例化对象时生成 this.base，若传参将参数转为 Date 对象，不传参为实例化时刻的 Date 对象
-	reset(time) { return this.base = new Date(time == null ? Date.now() : time); } // 类似于重新实例化的方法，但可以直接修改 this.base，返回重设后的 Date 对象
+class Timer { // class 版本
+	constructor(time) { this.base = new Date(time ?? Date.now()); } // 实例化对象时生成 this.base，若传参将参数转为 Date 对象，不传参为实例化时刻的 Date 对象
+	reset(time) { return this.base = new Date(time ?? Date.now()); } // 类似于重新实例化的方法，但可以直接修改 this.base，返回重设后的 Date 对象
 	timing(time) { // 计时，返回 this.base 与 this.time 的时间间隔
-		this.time = new Date(time == null ? Date.now() : time); // 生成 this.time
-		var ms = Math.max(this.time, this.base) - Math.min(this.time, this.base), sec = ms / 1000, min = sec / 60, hour = min / 60, day = hour / 24; // 处理时间间隔
-		return [Math.floor(day), Math.floor(hour % 24), Math.floor(min % 60), Math.floor(sec % 60), ms % 1000, this.time < this.base]; // 返回时间间隔： [日,时,分,秒,毫秒,是否为倒计时]
+		this.time = new Date(time ?? Date.now()); // 生成 this.time
+		const isCountdown = this.time < this.base,
+			ms = isCountdown ? this.base - this.time : this.time - this.base, sec = ms / 1000, min = sec / 60, hour = min / 60, day = hour / 24; // 处理时间间隔
+		return {
+			days: Math.floor(day),
+			hours: Math.floor(hour % 24),
+			minutes: Math.floor(min % 60),
+			seconds: Math.floor(sec % 60),
+			milliseconds: ms % 1000,
+			isCountdown
+		};
 	}
 }
--- timer/function.js
+++
@@ -1,8 +0,0 @@
-if (!Date.now) { Date.now = function () { return +new Date; }; } // ECMA-262 第五版标准中的 Date.now polyfill
-function Timer(time) { this.base = new Date(time == null ? Date.now() : time); } // 构造函数版本，相关注释请见 class 版本
-Timer.prototype.reset = function (time) { return this.base = new Date(time == null ? Date.now() : time); };
-Timer.prototype.timing = function (time) {
-	this.time = new Date(time == null ? Date.now() : time);
-	var ms = Math.max(this.time, this.base) - Math.min(this.time, this.base), sec = ms / 1000, min = sec / 60, hour = min / 60, day = hour / 24;
-	return [Math.floor(day), Math.floor(hour % 24), Math.floor(min % 60), Math.floor(sec % 60), ms % 1000, this.time < this.base];
-};
